d4d0d8aa668730454985d7d446d5f56db2e38535
Unknown <dlcmariano@gmail.com>
2020-04-10 19:31:35 -0300

    Name change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 const Footer = (props) =>{
     return(
         <footer>
-            <p>&copy; 2017 <strong>Aero Rewards</strong> - Aerolab</p>
+            <p>&copy; 2020 <strong>Mariano</strong>de la Cruz</p>
         </footer>
     )
 };
